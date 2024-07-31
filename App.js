8b4b934c61c2fa733f06df7a57447dfc9a472b92
Laura Ogbechie <ogbechielaura04@gmail.com>
2024-07-31 15:08:50 +0100

    created button component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ButtonComp from './src/components/button';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
+      <ButtonComp title={'Bad habit'} buttonSpacing={styles.buttonSpace}/>
     </View>
   );
 }
@@ -17,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonSpace: {
+    paddingHorizontal: 50.5,
+    paddingVertical: 10
+  }
 });
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,1 +1,29 @@
-import React from "react";
+import React from 'react';
+import { Button, StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+function ButtonComp({ title, buttonSpacing }) {
+  const { button, buttonText } = styles;
+  return (
+    <>
+      <TouchableOpacity style={[button, buttonSpacing]}>
+        <Text style={buttonText}>{title}</Text>
+      </TouchableOpacity>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'green',
+    borderRadius: 71,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+    lineHeight: 24,
+    letterSpacing: 0.64
+  }
+});
+
+export default ButtonComp;
